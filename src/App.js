125c3619f349f09e17f079602d0dc54d331b5b0a
Tauheed Mukhtar <tauheed.mukhtar@gmail.com>
2020-03-19 00:57:54 +0500

    added header component and styling

--- src/App.js
+++ src/App.js
@@ -5,20 +5,23 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
-    <BrowserRouter>
-    <div>
-      
-      <Link to='/shop'> SHOP HERE </Link>
     
-      <Route exact path='/' component={HomePage} />
-      <Route path='/shop' component={ShopPage} />
-      <Route path='/shop/hats' component={Hats} />
-      <Route path='/shop/sneakers' component = {Sneakers} />
-
-    </div>
+    <BrowserRouter>
+      
+      <Header />
+    
+      <div>   
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+        <Route path='/shop/hats' component={Hats} />
+        <Route path='/shop/sneakers' component = {Sneakers} />
+        <Route path='/contact' component = {Contact} />
+        <Route path='/cart' component = {Cart} />
+      </div>
     </BrowserRouter>
   );
 }
@@ -36,4 +39,14 @@ const Sneakers = () => (
   </div>
 )
 
+const Contact = () => (
+  <div className = 'homepage'>
+      <h1>Contact Page</h1>        
+  </div>
+)
+const Cart = () => (
+  <div className = 'homepage'>
+      <h1>Your Cart</h1>        
+  </div>
+)
 export default App;
